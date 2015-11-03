04288a812ee4a5098258b95046f53aee498bf352
mndvns <mail@mndvns.com>
2015-11-03 08:28:55 -0700

    add warning on undefined displayName

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ module.exports = function(source) {
   this.cacheable && this.cacheable();
 
   var res = this.resourcePath;
-  var filename = path.basename(res, path.extname(res));
+  var filename = JSON.stringify(path.basename(res, path.extname(res)));
 
   return source +
     '\n\n' +
-    'if (!exports.displayName) exports.displayName = ' + JSON.stringify(filename) + '\n\n' +
+    'if (!exports.displayName) {\n' +
+    '  console.warn(\'displayName not set on component ' + filename + '\');\n' +
+    '  exports.displayName = ' + filename + ';\n' +
+    '}\n\n' +
     template.replace(/__FILENAME__/g, JSON.stringify(res));
 };
